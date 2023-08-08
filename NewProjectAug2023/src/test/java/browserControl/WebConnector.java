8034cf67b03c7797683d98e30c88eded28f2a1c8
Dipali152522 <137784432+Dipali152522@users.noreply.github.com>
2023-08-08 10:52:08 +0100

    Prep for writing testing scenarios

---
+++ NewProjectAug2023/src/test/java/browserControl/WebConnector.java
@@ -0,0 +1,31 @@
+package browserControl;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import utilities.ConstantUtils;
+
+public class WebConnector {
+    public static WebDriver driver;
+
+    public static void openBrowser() {
+        if (ConstantUtils.BROWSER_NAME.equalsIgnoreCase("firefox")) {
+            FirefoxOptions fo = new FirefoxOptions();
+            fo.addArguments("start-maximized");
+            driver = new FirefoxDriver(fo);
+        } else {
+            ChromeOptions co = new ChromeOptions();
+            co.addArguments("start-maximized");
+            driver = new ChromeDriver(co);
+        }
+//        if using implicit wait it has to be defined here
+//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+    }
+
+    public static void closeBrowser() {
+        driver.quit();
+    }
+}
---
+++ NewProjectAug2023/src/test/java/utilities/ConstantUtils.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class ConstantUtils {
+    public static String Base_URL = "https://www.saucedemo.com";
+
+//    Browser Name Chrome or Firefox
+    public static String BROWSER_NAME = "CHROME";
+
+//    Browser Type Normal/Headless
+    public static String BROWSER_TYPE = "Normal";
+}
